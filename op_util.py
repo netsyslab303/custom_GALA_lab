394c76a078cd7034aaaf59de01e0d2ef5de8c619
lee <ws_lee@chungbuk.ac.kr>
2023-07-06 10:24:57 +0900

    [Update] Input 과 Output의 유클리디안 거리 계산 코드 추가

--- op_util.py
+++ op_util.py
@@ -1,4 +1,6 @@
 import copy
+import math
+import random
 
 import tensorflow as tf
 import numpy as np
@@ -7,6 +9,39 @@ import scipy.sparse as ss
 import sklearn.metrics as sklm
 import cv2
 from nets import SVD
+import train_ws
+
+
+def norm_euclidean_distance(input, output, acc):
+    dist = np.linalg.norm(input[acc] - output[acc])
+    return dist
+
+
+def euclidean_distance(input, output, acc):
+    dist = 0
+    input[acc][0] *= 1920
+    input[acc][1] *= 1080
+    output[acc][0] *= 1920
+    output[acc][1] *= 1080
+    dist += np.linalg.norm(input[acc] - output[acc])
+    return dist
+
+
+# def norm_euclidean_distance(input, output, acc):
+#     dist = 0
+#     for i in range(17):
+#         dist += np.linalg.norm(input[i]-output[i])
+#     return dist
+#
+# def euclidean_distance(input, output, acc):
+#     dist = 0
+#     for i in range(17):
+#         input[i][0] *= 1920
+#         input[i][1] *= 1080
+#         output[i][0] *= 1920
+#         output[i][1] *= 1080
+#         dist += np.linalg.norm(input[i]-output[i])
+#     return dist
 
 def Optimizer(model, LR):
     with tf.name_scope('Optimizer_w_Distillation'):
@@ -20,68 +55,76 @@ def Optimizer(model, LR):
 
     l = 5e1
     mu = 1.
+
     @tf.function
-    def training(input, weight_decay, k):
+    def training(input, noised_input, weight_decay, k):
         with tf.GradientTape() as tape:
-            noised_input = copy.deepcopy(input)
-            noised_input[:,9,:] = 0
-            generated = model(noised_input, training = True)
-            # 모델 입력은 noise 추가, loss 계산은 원본 Pkl.
-            loss = tf.reduce_sum(tf.square(input - generated))/2/input.shape[0]
+            generated = model(noised_input, training=True)
+            loss = tf.reduce_sum(tf.square(input - generated)) / 2 / input.shape[0]
             total_loss = loss
             if weight_decay > 0.:
-                total_loss += tf.add_n([tf.reduce_sum(tf.square(v))*weight_decay/2 for v in model.trainable_variables])
+                total_loss += tf.add_n(
+                    [tf.reduce_sum(tf.square(v)) * weight_decay / 2 for v in model.trainable_variables])
         # gradient 계
         gradients = tape.gradient(total_loss, model.trainable_variables)
         # 모델 업데이트
         optimizer.apply_gradients(zip(gradients, model.trainable_variables))
         train_loss.update_state(loss)
 
-    #@tf.function
+    # @tf.function
     def finetuning(input, weight_decay, k):
         with tf.GradientTape() as tape:
-            generated = model(input, training = True)
-            loss = tf.reduce_sum(tf.square(input - generated))/2/input.shape[0]
+            generated = model(input, training=True)
+            loss = tf.reduce_sum(tf.square(input - generated)) / 2 / input.shape[0]
 
             H = model.H
-            HHT = tf.matmul(H,H,transpose_a=True)
-            s,u,v = SVD.SVD(tf.expand_dims(mu*tf.eye(HHT.shape[0])+l*HHT,0), k)
-            scc_loss = mu*l/2*tf.linalg.trace(tf.matmul(tf.squeeze(tf.matmul(v, u/tf.reshape(s,[1,1,k]), transpose_b = True)),HHT))
+            HHT = tf.matmul(H, H, transpose_a=True)
+            s, u, v = SVD.SVD(tf.expand_dims(mu * tf.eye(HHT.shape[0]) + l * HHT, 0), k)
+            scc_loss = mu * l / 2 * tf.linalg.trace(
+                tf.matmul(tf.squeeze(tf.matmul(v, u / tf.reshape(s, [1, 1, k]), transpose_b=True)), HHT))
             total_loss = loss + scc_loss
 
             if weight_decay > 0.:
-                total_loss += tf.add_n([tf.reduce_sum(tf.square(v))*weight_decay for v in model.trainable_variables])
+                total_loss += tf.add_n([tf.reduce_sum(tf.square(v)) * weight_decay for v in model.trainable_variables])
 
         gradients = tape.gradient(total_loss, model.trainable_variables)
         optimizer_tune.apply_gradients(zip(gradients, model.trainable_variables))
         train_loss.update_state(loss)
 
     def validate(input, labels, k):
-        H = model(input, training = False)
+        H = model(input, training=False)
         H = H.numpy()
-        cv2.imwrite('test2/rec0.png',(np.clip(np.hstack(list(np.hstack(list(H[:400].reshape(20,20,28,28))))),0,1)*255).astype(np.uint8))
-        cv2.imwrite('test2/rec1.png',(np.clip(np.hstack(list(np.hstack(list(H[-400:].reshape(20,20,28,28))))),0,1)*255).astype(np.uint8))
-        cv2.imwrite('test2/ori.png',(np.clip(np.hstack(list(np.hstack(list(input[:400].reshape(20,20,28,28))))),0,1)*255).astype(np.uint8))
-        cv2.imwrite('test2/ori2.png',(np.clip(np.hstack(list(np.hstack(list(input[-400:].reshape(20, 20, 28, 28))))), 0, 1) * 255).astype(np.uint8))
+        cv2.imwrite('test2/rec0.png',
+                    (np.clip(np.hstack(list(np.hstack(list(H[:400].reshape(20, 20, 28, 28))))), 0, 1) * 255).astype(
+                        np.uint8))
+        cv2.imwrite('test2/rec1.png',
+                    (np.clip(np.hstack(list(np.hstack(list(H[-400:].reshape(20, 20, 28, 28))))), 0, 1) * 255).astype(
+                        np.uint8))
+        cv2.imwrite('test2/ori.png',
+                    (np.clip(np.hstack(list(np.hstack(list(input[:400].reshape(20, 20, 28, 28))))), 0, 1) * 255).astype(
+                        np.uint8))
+        cv2.imwrite('test2/ori2.png', (
+                    np.clip(np.hstack(list(np.hstack(list(input[-400:].reshape(20, 20, 28, 28))))), 0, 1) * 255).astype(
+            np.uint8))
         H = model.H
         latent = H.numpy()
 
-        u,s,v = np.linalg.svd(mu*np.eye(latent.shape[1]) + l*np.matmul(latent.T, latent), full_matrices = False)
-        H_inv = np.matmul(v[:k].T, (u[:,:k]/np.expand_dims(s[:k],0)).T)
-        A_latent = l*np.matmul(np.matmul(latent, H_inv), latent.T)
-        A_latent = np.maximum(0,A_latent)
-        #ss.save_npz('test2/test_full.npz', ss.csr_matrix(A_latent))
-        print ('Optimal A is computes')
+        u, s, v = np.linalg.svd(mu * np.eye(latent.shape[1]) + l * np.matmul(latent.T, latent), full_matrices=False)
+        H_inv = np.matmul(v[:k].T, (u[:, :k] / np.expand_dims(s[:k], 0)).T)
+        A_latent = l * np.matmul(np.matmul(latent, H_inv), latent.T)
+        A_latent = np.maximum(0, A_latent)
+        # ss.save_npz('test2/test_full.npz', ss.csr_matrix(A_latent))
+        print('Optimal A is computes')
 
-        num_labels = np.max(labels)+1
+        num_labels = np.max(labels) + 1
 
-        clustering = SpectralClustering(n_clusters = num_labels, affinity = 'precomputed')
+        clustering = SpectralClustering(n_clusters=num_labels, affinity='precomputed')
         prediction = clustering.fit(A_latent)
 
         results = prediction.labels_
 
         total_true = 0
-        vote_box = {c:0 for c in range(num_labels)}
+        vote_box = {c: 0 for c in range(num_labels)}
         for i in range(num_labels):
             matched = labels[results == i]
             for m in matched:
@@ -102,7 +145,7 @@ def Optimizer(model, LR):
             for v in vote_box.keys():
                 vote_box[v] = 0
 
-        acc = total_true/labels.shape[0]
+        acc = total_true / labels.shape[0]
 
         nmi_score = sklm.adjusted_mutual_info_score(labels.reshape(-1), results.reshape(-1))
         ari_score = sklm.adjusted_rand_score(labels.reshape(-1), results.reshape(-1))
@@ -111,17 +154,23 @@ def Optimizer(model, LR):
         ARI.update_state(ari_score)
 
     def validate2(input, k):
-        H = model(input, training=False)
-        print("------------------")
-        print("input")
-        print(input[0])
-        print(H[0].numpy())
+        noised, acc_rnd = train_ws.make_noise(input)
+        H = model(noised, training=False)
+        norm_dist = 0
+        dist = 0
+        for i in range(100):
+            # print("------------------")
+            # print(acc_rnd[i])
+            # print("Original")
+            # print(input[i])
+            # print("Noising")
+            # print(noised[i])
+            # print("Denoising")
+            # print(H[i].numpy())
+            output = H[i].numpy()
+            norm_dist += norm_euclidean_distance(input[i], output, acc_rnd[i])
+            dist += euclidean_distance(input[i], output, acc_rnd[i])
+        print(norm_dist / 100)
+        print(dist / 100)
 
     return training, train_loss, finetuning, validate2, ACC, NMI, ARI
-
-
-
-
-
-
-
