dde0a801fe0e7bc9f55749a49c572bff00010c22
lee <ws_lee@chungbuk.ac.kr>
2023-08-18 14:03:25 +0900

    [Update] 극좌표계 데이터셋 구성 추가

--- gen_bone_data.py
+++ gen_bone_data.py
@@ -5,6 +5,18 @@ import argparse
 import train_ws_seperated_temporal
 import numpy as np
 
+
+def dec_to_polar(bone):
+    polar_bone = copy.deepcopy(bone)
+    x = polar_bone[:, :, 0]
+    y = polar_bone[:, :, 1]
+    polar_dis = np.sqrt(x ** 2 + y ** 2)
+    polar_deg = np.arctan2(y, x)
+    polar_deg[polar_deg < 0] += 2 * np.pi
+    polar_bone[:, :, 0] = polar_dis
+    polar_bone[:, :, 1] = polar_deg/(2 * np.pi)
+    return polar_bone
+
 parser = argparse.ArgumentParser()
 parser.add_argument("--time_input", default=30, type=str)
 parser.add_argument("--frame_interval", default=15, type=int)
@@ -15,13 +27,13 @@ neighbor_link = [(15, 13), (13, 11), (16, 14), (14, 12), (11, 5), (12, 6),
                  (1, 0), (3, 1), (2, 0), (4, 2)]
 
 home_path = os.path.dirname(os.path.abspath(__file__))
-#pkl_file = home_path + '/normalized_input/Normalized_bone_30_30.pkl'
+# pkl_file = home_path + '/normalized_input/Normalized_bone_30_30.pkl'
 pkl_file = home_path + '/ntu60_hrnet.pkl'
 with open(pkl_file, 'rb') as f:
     data = pickle.load(f)
 
 bone_data = []
-org_data = []
+polar_data = []
 time_input = args.time_input
 frame_interval = args.frame_interval
 
@@ -39,9 +51,16 @@ for data_len in range(len(data['annotations'])):
         for num1 in range(0, len(bone), frame_interval):
             end = num1 + time_input
             if end <= len(bone):
-                bone = bone[num1:end]
-                score = data['annotations'][data_len]['keypoint_score'][0][num1:end].reshape(bone.shape[0],bone.shape[1],1)
-                bone_data.append(np.concatenate((bone, score), axis=2))
+                score = data['annotations'][data_len]['keypoint_score'][0][num1:end].reshape(bone[num1:end].shape[0],
+                                                                                             bone[num1:end].shape[1], 1)
+                bone_data.append(np.concatenate((bone[num1:end], score), axis=2))
+                polar_data.append(np.concatenate((dec_to_polar(bone[num1:end]), score), axis=2))
 
-with open(home_path + '/normalized_input/Normalized_bone_score_{}_{}.pkl'.format(time_input,frame_interval), 'wb') as f:
+with open(home_path + '/normalized_input/Normalized_bone_score_{}_{}.pkl'.format(time_input, frame_interval),
+          'wb') as f:
     pickle.dump(bone_data, f, pickle.HIGHEST_PROTOCOL)
+
+with open(home_path + '/normalized_input/Normalized_polar_bone_score_{}_{}.pkl'.format(time_input, frame_interval),
+          'wb') as f:
+    pickle.dump(polar_data, f, pickle.HIGHEST_PROTOCOL)
+
