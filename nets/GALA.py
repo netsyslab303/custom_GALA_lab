1e9fb09fd8a95d4b59858317c57fd7ce1b41e4f4
lee <ws_lee@chungbuk.ac.kr>
2023-08-18 14:02:51 +0900

    [Update] model layer 확장 및 구조 변경

--- nets/GALA.py
+++ nets/GALA.py
@@ -5,27 +5,32 @@ import train_ws_seperated_temporal
 from tensorflow import keras
 
 use_dyna_adj = train_ws_seperated_temporal.use_dyna_adj
+input_size = train_ws_seperated_temporal.input_size
+
 
 class Model(tf.keras.Model):
     def __init__(self, s_DAD, t_DAD, name='GALA', batch_size=64, trainable=True, time_input=15, **kwargs):
         super(Model, self).__init__(name=name, **kwargs)
-
+        D = [64, 64, 128, 128, 256, 256]
         self.batch_size = batch_size
         self.GALA = {}
-        D = [400, 300, 100]
-        inputs = keras.Input(shape=(17*time_input, 2,), name="digits")
+        self.model_len = len(D)
+        inputs = keras.Input(shape=(17 * time_input, input_size,), name="digits")
         for i, d in enumerate(D):
             if i == 0:
                 self.GALA['enc%d' % i] = tf.keras.layers.Dense(d, trainable=trainable)  # , use_bias = False)
                 self.GALA['enc%d' % i](inputs)
             else:
                 self.GALA['enc%d' % i] = tf.keras.layers.Dense(d, trainable=trainable)  # , use_bias = False)
-                self.GALA['enc%d' % i](keras.Input(shape=(17*time_input, D[i - 1],)))
+                self.GALA['enc%d' % i](keras.Input(shape=(17 * time_input, D[i - 1],)))
 
         if trainable:
-            for i, d in enumerate(D[1::-1] + [2]):
+            for i, d in enumerate(D[self.model_len:0:-1] + [2]):
                 self.GALA['dec%d' % i] = tf.keras.layers.Dense(d, trainable=trainable)  # , use_bias = False)
-                self.GALA['dec%d' % i](keras.Input(shape=(17*time_input, D[-(i + 1)],)))
+                if i == 0:
+                    self.GALA['dec%d' % i](keras.Input(shape=(17 * time_input, D[-1],)))
+                else:
+                    self.GALA['dec%d' % i](keras.Input(shape=(17 * time_input, D[-i],)))
 
         self.s_DADsm = tf.sparse.SparseTensor(s_DAD['DADsm_indices'], s_DAD['DADsm_values'][0], s_DAD['dense_shape'][0])
         self.s_DADsp = tf.sparse.SparseTensor(s_DAD['DADsp_indices'], s_DAD['DADsp_values'][0], s_DAD['dense_shape'][0])
@@ -37,7 +42,7 @@ class Model(tf.keras.Model):
         dad = tf.sparse.to_dense(DAD)
         if use_dyna_adj:
             dad = tf.math.multiply(dyna, dad)
-        output = tf.nn.relu(tf.matmul(dad,inputs))
+        output = tf.nn.relu(tf.matmul(dad, inputs))
         return output
 
     def Laplacian_sharpening(self, x, name, training, DAD, dyna):
@@ -49,15 +54,15 @@ class Model(tf.keras.Model):
         return output
 
     def call(self, H, training=None, dyna=None):
-        for i in range(3):
-            if i < 2:
-                H = self.Laplacian_smoothing(H, 'enc%d' % i, training, self.t_DADsm, dyna)
-            else:
+        for i in range(self.model_len):
+            if i % 2 == 0:
                 H = self.Laplacian_smoothing(H, 'enc%d' % i, training, self.s_DADsm, dyna)
-        self.H = H
-        for i in range(3):
-            if i == 0:
-                H = self.Laplacian_sharpening(H, 'dec%d' % i, training, self.s_DADsp, dyna)
             else:
+                H = self.Laplacian_smoothing(H, 'enc%d' % i, training, self.t_DADsm, dyna)
+        self.H = H
+        for i in range(self.model_len):
+            if i % 2 == 0:
                 H = self.Laplacian_sharpening(H, 'dec%d' % i, training, self.t_DADsp, dyna)
+            else:
+                H = self.Laplacian_sharpening(H, 'dec%d' % i, training, self.s_DADsp, dyna)
         return H
